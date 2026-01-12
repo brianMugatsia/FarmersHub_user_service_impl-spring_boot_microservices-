380bb7000d1917da0446d3d2e40d364336cb4c90
brian <mugastiabrian@gmai.com>
2026-01-13 00:18:52 +0300

    Updated user_service with SecurityConfig, Jwt, and controller changes

---
+++ src/main/java/com/farmershub/user_service/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.farmershub.user_service.service;
+
+import com.farmershub.user_service.entity.User;
+import com.farmershub.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singleton(() -> "ROLE_" + user.getRole()) // authorities
+        );
+    }
+}
